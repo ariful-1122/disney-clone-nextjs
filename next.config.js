e787751692bf1484e74c18d216fad14c16d1ac7a
ariful-1122 <greenheart970@gmail.com>
2021-05-06 16:49:53 +0600

    auth problem fix

--- next.config.js
+++ next.config.js
@@ -19,6 +19,9 @@ module.exports = () => {
         MONGO_URI:
           "mongodb+srv://admin:admin@cluster0.qbmgm.mongodb.net/disney-dev?retryWrites=true&w=majority",
         BASE_URL: "",
+        GOOGLE_CLIENT_ID:
+          "580352229470-20fke8ubv0eqg4c35635j2aph8rbubpd.apps.googleusercontent.com",
+        GOOGLE_CLIENT_SECRET: "95NafQv-zDw2eoKtFhTZKVdp",
       },
     };
   }
